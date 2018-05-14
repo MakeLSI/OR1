49d944d4bafaf69818280a34569aa22bb238ed14
Seijiro Moriyama <seijiro.moriyama@anagix.com>
2018-05-14 22:58:09 +0900

    lef_conv.rb 3rd cut: PATH to RECT conversion for port pins added; changed Y to YB in Sato-sans liberty file for exnr, inv1, inv2, inv4, inv8, na21, na212, na222, na31, na41, nr21, nr212, nr222, and nr31

--- lef_conv.rb
+++ lef_conv.rb
@@ -18,6 +18,58 @@ File.read(File.join(File.dirname(__FILE__),'./OR1_stdcells.lib')).each_line{|l|
 }
 # debugger
 
+def port_with_path layer, width, x1, y1, x2, y2
+    if y1 == y2
+      xll = x1.to_f
+      yll = y1.to_f - width.to_f/2.0
+      xur = x2.to_f
+      yur = y1.to_f + width.to_f/2.0
+    elsif x1 == x2
+      xll = x1.to_f - width.to_f/2.0
+      yll = y1.to_f
+      xur = x1.to_f + width.to_f/2.0
+      yur = y2.to_f
+    end
+    p =<<EOF
+    PORT
+      LAYER #{layer} ;
+        RECT #{xll} #{yll} #{xur} #{yur} ;
+    END
+EOF
+    p
+end
+
+def path_to_rect lines
+  layer = width = x1 = y1 = x2 = y2 = rest = nil
+  lines.each_line{|l|
+    if l=~/LAYER +(\S+)/
+      layer = $1
+    elsif l=~/WIDTH +(\S+)/
+      width = $1
+    elsif l=~/PATH +(\S+) +(\S+) +(\S+) +(\S+) +(.*);/
+      x1=$1
+      y1=$2
+      x2=$3
+      y2=$4
+      rest = $5.strip
+    end
+  }
+  p = port_with_path layer, width, x1, y1, x2, y2 
+  return p if rest.size == 0
+  x1 = x2
+  y1 = y2
+  while rest.size > 0
+    rest =~ /(\S+) +(\S+) *(.*)/
+    x2 = $1
+    y2 = $2
+    rest = $3
+    p << port_with_path(layer, width, x1, y1, x2, y2)
+    x1 = x2
+    y1 = y2
+  end
+  p
+end
+
 def read_lef lines
   pl = nil 
   cell = nil
@@ -64,18 +116,15 @@ def read_lef lines
       pin = $1 
       puts pl
       # puts "cell:#{cell}, pin:#{pin}"
-      dir = $direction[cell][pin]
+      dir = $direction[cell][pin] || 'INOUT' # for vdd, gnd, etc. 
       puts "    DIRECTION #{dir} ;" if dir
     elsif pl=~/USE SIGNAL/ 
       puts "    USE SIGNAL ;\n"
     elsif pl=~/END *#{pin}/
       ports.each_with_index{|p, i|
         if port_type[i] == 'PATH'
-          puts p
-        end
-      }
-      ports.each_with_index{|p, i|
-        if port_type[i] != 'PATH'
+          puts path_to_rect(p)
+        else
           puts p
         end
       }
@@ -105,6 +154,32 @@ def extract_macro name, lines
     end
   }
   result
+end
+
+if ARGV[0].nil?
+  lines =<<EOF
+    PORT
+      LAYER metal1 ;
+        WIDTH 1.000 ;
+        PATH 4.800 20.500 4.800 18.000 ;
+    END
+EOF
+  
+  puts path_to_rect(lines)
+  
+  debugger
+  
+  lines =<<EOF
+    PORT
+      LAYER metal1 ;
+        WIDTH 1.000 ;
+        PATH 4.800 20.500 4.800 18.000 7.500 18.000 7.500 6.500 6.800 6.500 6.800 3.800 ;
+    END
+EOF
+  
+  puts path_to_rect(lines)
+  
+  debugger
 end
 
 if File.directory? ARGV[0]
